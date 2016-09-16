8f73646fa54d0a8829ec35ae892ea11b942aa945
Edith Li <Edith Li>
2016-09-16 02:43:40 -0400

    Signed-off-by: Edith Li <Edith Li>

--- lightpy.py
+++ lightpy.py
@@ -1,17 +1,57 @@
-import spidev
+import RPi.GPIO as GPIO
 import time
 import os
  
-# Open SPI bus
-spi = spidev.SpiDev()
-spi.open(0,0)
+GPIO.setmode(GPIO.BCM)
+DEBUG = 1
  
-# Function to read SPI data from MCP3008 chip
-# Channel must be an integer 0-7
-def ReadChannel(channel):
-  adc = spi.xfer2([1,(8+channel)<<4,0])
-  data = ((adc[1]&3) << 8) + adc[2]
-  return data
+#read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
+def readadc(adcnum, clockpin, mosipin, misopin, cspin):
+        if ((adcnum > 7) or (adcnum < 0)):
+                return -1
+        GPIO.output(cspin, True)
+ 
+        GPIO.output(clockpin, False)  # start clock low
+        GPIO.output(cspin, False)     # bring CS low
+ 
+        commandout = adcnum
+        commandout |= 0x18  # start bit + single-ended bit
+        commandout <<= 3    # we only need to send 5 bits here
+        for i in range(5):
+                if (commandout & 0x80):
+                        GPIO.output(mosipin, True)
+                else:
+                        GPIO.output(mosipin, False)
+                commandout <<= 1
+                GPIO.output(clockpin, True)
+                GPIO.output(clockpin, False)
+ 
+        adcout = 0
+        # read in one empty bit, one null bit and 10 ADC bits
+        for i in range(12):
+                GPIO.output(clockpin, True)
+                GPIO.output(clockpin, False)
+                adcout <<= 1
+                if (GPIO.input(misopin)):
+                        adcout |= 0x1
+ 
+        GPIO.output(cspin, True)
+        
+        adcout >>= 1       # first bit is 'null' so drop it
+        return adcout
+ 
+# change these as desired - they're the pins connected from the
+# SPI port on the ADC to the Cobbler
+SPICLK = 18
+SPIMISO = 23
+SPIMOSI = 24
+SPICS = 25
+ 
+# set up the SPI interface pins
+GPIO.setup(SPIMOSI, GPIO.OUT)
+GPIO.setup(SPIMISO, GPIO.IN)
+GPIO.setup(SPICLK, GPIO.OUT)
+GPIO.setup(SPICS, GPIO.OUT)
  
 # Function to convert data to voltage level,
 # rounded to specified number of decimal places.
@@ -31,7 +71,7 @@ delay = 5
 while True:
  
   # Read the light sensor data
-  light_level = ReadChannel(light_channel)
+  light_level = readadc(light_channel, SPICLK, SPIMOSI, SPIMISO, SPICS)
   light_volts = ConvertVolts(light_level,2)
  
  
